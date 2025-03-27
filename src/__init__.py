95a01b0a58ba1e626bd4bba448c112c3127f9193
Rasmus Duus <s214262@dtu.dk>
2025-03-27 12:45:37 +0100

    Loading even more data

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def load_resp(path_resp):
     """Loads response data from an .opt file and applies time filtering."""
@@ -17,6 +18,27 @@ def load_resp(path_resp):
     T = data['aero_thrust'].values
     
     return V, phi, omega, P, T
+
+def load_blade_data(file_path):
+    # Read the file, skipping the first few lines if necessary, and use whitespace as separator
+    data = pd.read_csv(file_path, sep='\s+', header=None, skiprows=6)  # Skip the first row with non-relevant info
+
+    # Rename columns to a more convenient format
+    data.columns = ['BlSpn', 'BlCrvAC', 'BlSwpAC', 'BlCrvAng', 'BlTwist', 'BlChord', 'BlAFID', 'BlCb', 'BlCenBn', 'BlCenBt']
+    
+    # Extract relevant data
+    BlSpn = data['BlSpn'].values
+    BlCrvAC = data['BlCrvAC'].values
+    BlSwpAC = data['BlSwpAC'].values
+    BlCrvAng = data['BlCrvAng'].values
+    BlTwist = data['BlTwist'].values
+    BlChord = data['BlChord'].values
+    BlAFID = data['BlAFID'].values
+    BlCb = data['BlCb'].values
+    BlCenBn = data['BlCenBn'].values
+    BlCenBt = data['BlCenBt'].values
+    
+    return BlSpn, BlCrvAC, BlSwpAC, BlCrvAng, BlTwist, BlChord, BlAFID, BlCb, BlCenBn, BlCenBt
 
 
 def plot_V_vs_phi(V, phi):
